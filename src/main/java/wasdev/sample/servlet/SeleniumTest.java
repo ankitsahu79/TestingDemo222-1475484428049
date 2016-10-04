053edb0da712cae8a96080c8fca0682897f9ce5b
ankit <ankisahu@.in.ibm.com>
2016-10-04 15:01:29 +0530

    Git

---
+++ src/main/java/wasdev/sample/servlet/SeleniumTest.java
@@ -0,0 +1,41 @@
+package wasdev.sample.servlet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+public class SeleniumTest {
+	static WebDriver wd;
+	static WebElement we;
+	
+	static void openBrowser() throws InterruptedException {
+		ProfilesIni pi = new ProfilesIni();
+		FirefoxProfile fp = pi.getProfile("default");
+
+		wd=new FirefoxDriver(fp);
+		wd.get("http://www.gmail.com");
+		WebElement we = wd.findElement(By.name("Email"));
+		
+		we.sendKeys("surajprakashbehera92@gmail.com");
+	
+		WebElement ws=wd.findElement(By.name("signIn"));
+		ws.click();
+		Thread.sleep(5000);
+		WebElement wp = wd.findElement(By.name("Passwd"));
+        wp.sendKeys("radhagiridhari");
+        
+        WebElement ws1=wd.findElement(By.xpath(".//*[@id='signIn']"));
+		ws1.click();
+		//wd.findElement(By.linkText("IBMSales")).click();
+		//wd.findElement(By.linkText("New proposal")).click();
+
+		}
+	public static void main(String suraj[]) throws InterruptedException
+	{
+	System.out.println("pankaj is teaching me");
+	openBrowser();
+	}
+	
+}
